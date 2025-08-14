1a192f9ca6cbcc9b3ceffb21e517e27523cb2e07
ronaliuzhong <liuzhong.r@northeastern.edu>
2025-08-14 12:40:51 -0400

    javascript

---
+++ script.js
@@ -0,0 +1,50 @@
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.disc&api_key=83354a297ca899d92d7aae28845a331c&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=83354a297ca899d92d7aae28845a331c&query=';
+
+const main = document.getElementById("section");
+const form = document.getElementById("form");
+const search = document.getElementById("query");
+
+returnMovies(APILINK);
+
+function returnMovies(url) {
+    fetch(url).then(res => res.json())
+    .then(function(data) { 
+        console.log(data.results);
+        data.results.forEach(element => {
+            const div_card = document.createElement('div');
+            const div_row = document.createElement('div');
+            const div_column = document.createElement('div');
+            const image = document.createElement('img');
+            const title = document.createElement('h3');
+            const center = document.createElement('center');
+
+            title.innerHTML = '${element.title)';
+            image.src = IMG_PATH + element.poster_path;
+
+            center.appendChild(image);
+            div_card.appendChild(center);
+            div_card.appendChild(title);
+            div_column.appendChild(div_card);
+            div_row.appendChild(div_column);
+            main.appendChild(div_row);
+        });
+    });
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+
+    const searchItem = search.value;
+    
+    if (searchItem) {
+        returnMovies(SEARCHAPI + searchItem);
+        search.value = '';
+    }
+
+});
+
+
+
